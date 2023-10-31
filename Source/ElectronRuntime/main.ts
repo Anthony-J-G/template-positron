36ba0bc45b8847865a1333f4190e3d7e3e286d7e
Anthony <61360659+Anthony-J-G@users.noreply.github.com>
2023-10-31 13:58:10 -0400

    Re-ordered some projects

---
+++ Source/ElectronRuntime/main.ts
@@ -0,0 +1,106 @@
+import { app, shell, session, ipcMain, BrowserWindow, globalShortcut, Menu, MenuItem } from "electron";
+import * as os from "os";
+import * as fs from "fs";
+import * as path from "path";
+import * as url from "url";
+
+
+
+var win: BrowserWindow;
+var isRelease: boolean = false;
+
+function createApp() {
+
+  //Browser Window common options
+  let browserOptions = { 
+    icon: path.join(__dirname, '../src/assets/icon/png/64x64.png'), 
+    title: 'OoT Randomizer GUI', 
+    opacity: 1.00, 
+    backgroundColor: '#000000', 
+    minWidth: 880, 
+    minHeight: 680, 
+    width: 961, 
+    height: 888,
+    show: false, 
+    webPreferences: { 
+      sandbox: false, 
+      nodeIntegration: false, 
+      contextIsolation: true, 
+      webviewTag: false, 
+    } 
+  };
+
+  //Override menu (only need dev tools shortcut)
+  let appMenu = new Menu();
+  let subMenu = new Menu();
+
+  subMenu.append(new MenuItem({
+    label: 'Toggle Developer Tools',
+    accelerator: os.platform() === 'darwin' ? 'Alt+Cmd+I' : 'Ctrl+Shift+I',
+    click: () => { win.webContents.openDevTools(); }
+  }));
+
+  subMenu.append(new MenuItem({
+    label: 'Refresh GUI',
+    accelerator: 'F5',
+    click: () => { win.webContents.reload(); }
+  }));
+
+
+  win = new BrowserWindow(browserOptions);
+  win.setMenu(appMenu);
+
+  if (!isRelease) {
+    win.loadURL("http://localhost:4200/"); //Dev server
+  }
+  else { //Load release dist
+
+    //Check if it exists first
+    let indexPath = path.join(__dirname, '../../Binaries/GUI/index.html');
+
+    if (!fs.existsSync(indexPath)) {
+
+      console.error("No release found");
+
+      setTimeout(() => {
+        app.quit();
+      }, 0);
+
+      throw Error("No Electron GUI found! Did you compile it properly?");
+    }
+
+    win.loadURL(
+      indexPath
+    );
+  }
+
+  win.once('ready-to-show', () => {
+    win.show();
+
+    if (!isRelease && !win.isMaximized()) //Open dev tools automatically if dev mode and not maximized
+      win.webContents.openDevTools();
+  });
+
+}
+
+//LISTENERS
+app.on("ready", createApp);
+
+//macOS exclusive, handles soft re-launches
+app.on("activate", () => {
+  if (win === null) {
+    createApp();
+  }
+});
+
+app.on("window-all-closed", () => {
+
+  //Ensures the electron process always shuts down properly if all windows have been closed
+  //Don't do this on macOS as users expect to be able to re-launch the app quickly from the dock after all windows get closed
+  if (os.platform() != "darwin") {
+
+    setTimeout(() => {
+      app.quit();
+    }, 1000);
+  }
+});
